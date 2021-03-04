b396b4cc4c968bdaca6dbc89e5f7a3a7e614140f
jessicavc <jessica.vaicav@gmail.com>
2021-03-04 16:12:11 -0500

    commit often

--- assets/js/init.js
+++ assets/js/init.js
@@ -2,5 +2,6 @@
   $(function () {
     $(".sidenav").sidenav();
     $(".parallax").parallax();
+    $(".button-collapse").sideNav();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
